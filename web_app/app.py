74ad549703b41411c2dfa173166b64da07cc52d6
Glitch (meal-maverick) <none>
2024-10-09 01:30:11 +0000

    🎥🏵 Checkpoint ./web_app/app.py:70263072/71

--- web_app/app.py
+++ web_app/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///meals.db'
 
 @app.route('/')
 def home():
