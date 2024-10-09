c60b3262049c14e519396f9e3d2a58572880c5c5
Glitch (meal-maverick) <none>
2024-10-09 19:20:19 +0000

    Updated script for progress bars ./web_app/static/css/style.css:70263072/316 ./web_app/templates/menu.html:70263072/320 ./web_app/static/js/budgetBar.js:70263072/830

--- web_app/static/js/budgetBar.js
+++ web_app/static/js/budgetBar.js
@@ -1,6 +1,9 @@
-        // JavaScript to handle progress bar updates
-        const mealForm = document.getElementById('mealForm');
-        const progressBar = document.getElementById('progressBar');
+document.addEventListener('DOMContentLoaded', function() { 
+// JavaScript to handle progress bar updates
+            const mealForm = document.getElementById('mealForm');
+            const progressBar = document.getElementById('progressBar');
+            const budgetElement = document.getElementById("budget");
+            const maxValue = parseFloat(budgetElement.getAttribute("data-budget"));
 
         mealForm.addEventListener('change', function() {
             let totalValue = 0;
@@ -16,13 +19,15 @@
               const servings = parseInt(document.getElementById(servingsId).value); // Get the number of servings
                 
                 totalValue += unitPriceValue * servings; // Multiply price by servings
+              console.log("change")
             });
 
             // Assume maximum value for progress is $100 (can be adjusted)
-            const maxValue = parseFloat(document.getElementById("budget").getAttribute("data-budget"));
-            const percentage = 100;//(totalValue / maxValue) * 100;
+            const percentage = (totalValue / maxValue) * 100;
 
             // Update progress bar
             progressBar.style.width = percentage + '%';
             progressBar.textContent = "100%"//Math.round(percentage) + '%'; // Show percentage text
+          console.log("loaded")
         });
+   });
